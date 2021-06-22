58ce969c11d094fb09953324bdea5cf9248097bf
wen <alwjlola@gmail.com>
2021-06-22 10:44:35 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # FlowBus
+![](https://jitpack.io/v/lwj1994/flowbus.svg)
--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,15 @@ buildscript {
     }
 }
 
+subprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven ("https://jitpack.io")
+    }
+}
+
+
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
